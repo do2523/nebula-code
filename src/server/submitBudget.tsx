db4b412a6d43ca4e80231a3e89aca5730d82bd8a
Hector Gonzalez <Hector.Gonzalez049@mymdc.net>
2024-09-28 16:13:17 -0400

    refactor

--- src/server/submitBudget.tsx
+++ src/server/submitBudget.tsx
@@ -1,5 +1,8 @@
 'use server'
-export default async function submitBudgetPreferences(e: React.SyntheticEvent) {
+
+import { Category } from "note/app/_components/BudgetingCategorySelector";
+
+export default async function submitBudgetPreferences(e: React.SyntheticEvent,categories: Category[]) {
 	const formData = await e;
 
 	const financialData = {
