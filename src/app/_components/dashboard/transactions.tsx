b5849d3b997c43cf938fc8f8438825ad223e10ba
Dylan Moo Hernandez <dylangmooh@gmail.com>
2024-09-28 12:18:08 -0400

    responsive card layout

---
+++ src/app/_components/dashboard/transactions.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Transactions() {
+  return (
+  <div>
+    <h1 className="p-6 bg-[#171D18] rounded-2xl h-48 text-white text-2xl font-bold">Transaction History</h1>
+  </div>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import Navbar from '../_components/navbar';
+import Spending from '../_components/dashboard/spending';
+import Transactions from '../_components/dashboard/transactions';
+import Stats from '../_components/dashboard/stats';
 
 export default function Dashboard() {
   return (
-  <div>
-    <h1 className="text-white">Dashboard</h1>
-    <p>Welcome to the dashboard page!</p>
-  </div>
-  )
+    <div>
+      <Navbar />
+      <div className="h-min mt-8 pt-20 ml-8 mr-8">
+        <div className="grid grid-cols-1 gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {/* Overall Spending */}
+          <div className="col-span-1 md:col-span-2">
+            <Spending />
+          </div>
+
+          {/* Stats (4 mini cards) */}
+          <div className="col-span-1">
+            <Stats />
+          </div>
+
+          {/* Transaction History */}
+          <div className="col-span-1 md:col-span-3">
+            <Transactions />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
