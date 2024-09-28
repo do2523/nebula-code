b6855315a81b363d84fc365823e9bd8080da3d0f
L <luparedes100@gmail.com>
2024-09-28 12:25:21 -0400

    fix import error

--- src/n/components/ui/input.tsx
+++ src/n/components/ui/input.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-import { cn } from "n/lib/utils"
+import { cn } from "src/n/lib/utils"
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
