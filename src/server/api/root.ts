42a29e3e3d37ccd0adaac02d00059098ea8ae7a1
L <luparedes100@gmail.com>
2024-09-28 15:37:38 -0400

    added default values to financial data

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,8 @@
 import { postRouter } from "note/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "note/server/api/trpc";
 import { userRouter } from "./routers/user";
+import { financialData } from "../db/schema";
+import { financialDataRouter } from "./routers/financialData";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +12,7 @@ import { userRouter } from "./routers/user";
 export const appRouter = createTRPCRouter({
   post: postRouter,
   user: userRouter,
+  financialData: financialDataRouter,
 });
 
 // export type definition of API
--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -3,17 +3,11 @@ import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const userRouter = createTRPCRouter({
     getById: publicProcedure.input(z.string()).query(async ({ ctx, input }) => {
-        const user = await ctx.db.query.users.findFirst({
+        return await ctx.db.query.users.findFirst({
             where: (user, { eq }) => eq(user?.id, input),
             with: {
                 financialData: true
             },
         })
-
-        if(user == undefined) {
-            return null;
-        }
-
-        return user;
     }),
 })
