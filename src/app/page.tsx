2a7814514264b309fd6fa42dbcefdd26b19216b2
Dylan Moo Hernandez <dylangmooh@gmail.com>
2024-09-28 18:44:46 -0400

    New homepage and navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,24 @@ import { getServerAuthSession } from "note/server/auth";
 import { api, HydrateClient } from "note/trpc/server";
 import Homepage from "./_components/main_page";
 import Navbar from "./_components/navbar";
+import Dashboard from "./dashboard/page";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
   const session = await getServerAuthSession();
 
   void api.post.getLatest.prefetch();
-
-  return (
-    <>
-    <Navbar />
-    <Homepage />
-    </>
     
-  );
+    if (!session) {
+      return (
+        <>
+        <Homepage/>
+        </>
+      );
+    }
+    else
+    {
+      return <Dashboard/>
+    }
+
 }
