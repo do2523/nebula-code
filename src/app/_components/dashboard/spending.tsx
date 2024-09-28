ee8b8382f3dcfba748fc6c458b3fde8c03463f58
L <luparedes100@gmail.com>
2024-09-28 18:25:46 -0400

    added chart to dashboard

--- src/app/_components/dashboard/spending.tsx
+++ src/app/_components/dashboard/spending.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import Chart from './chart';
 
 export default function Spending() {
   return (
   <div className="p-6 col-span-1 md:col-span-2 flex flex-col h-full bg-[#171D18] rounded-2xl">
-    <h1 className="text-white text-xl font-bold">Overall Spending</h1>
-    <div className="bg-gray-100 mt-4 h-full">GRAPH</div>
+    <Chart />
   </div>
   );
 }
