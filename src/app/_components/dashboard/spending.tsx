133d9acaa9a365495398b79434c30418acfb7030
Dylan Moo Hernandez <dylangmooh@gmail.com>
2024-09-28 12:40:51 -0400

    new ui changes

--- src/app/_components/dashboard/spending.tsx
+++ src/app/_components/dashboard/spending.tsx
@@ -4,6 +4,7 @@ export default function Spending() {
   return (
   <div className="p-6 col-span-1 md:col-span-2 flex flex-col h-full bg-[#171D18] rounded-2xl">
     <h1 className="text-white text-2xl font-bold">Overall Spending</h1>
+    <div className="text-white">GRAPH</div>
   </div>
   );
 }
