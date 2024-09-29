7f4f41c0520e14590182b6c9f755a0cfce31ebd4
do262431@gmail.com <do262431@gmail.com>
2024-09-29 10:46:45 -0400

    k

--- next.config.js
+++ next.config.js
@@ -5,11 +5,13 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
-
-modules.exports = {
+const config = {
     eslint : {
         ignoreDuringBuilds: true
+    },
+    typescript: {
+        ignoreBuildErrors: true
     }
-}
+};
+
 export default config;
