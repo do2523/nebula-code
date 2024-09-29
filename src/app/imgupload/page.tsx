92e145867316042ad38d7cde53056a5d2b2d4bc7
do262431@gmail.com <do262431@gmail.com>
2024-09-28 23:14:18 -0400

    Image Recognition

--- src/app/imgupload/page.tsx
+++ src/app/imgupload/page.tsx
@@ -1,10 +1,10 @@
-// import ImageUploader from "../_components/ImageUploader";
+import ImageUploader from "../_components/ImageUploader";
 
 export default function Imgupload() {
     return (
     <>
     <h1>Hello World</h1>
-    {/* <ImageUploader /> */}
+    <ImageUploader />
     </>
     )
 }
